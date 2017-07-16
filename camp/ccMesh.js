ed49ab5ee163daf40cf5eadb5d40647a90cdff54
connorbell <connor.n.bell@gmail.com>
2017-07-16 12:46:05 -0300

    Added color and time uniform

--- camp/ccMesh.js
+++ camp/ccMesh.js
@@ -36,14 +36,14 @@ ccMesh.plane = function(x, y, size){
 	return geometry;
 }
 
-ccMesh.cone = function(radialSegments, depthSegments, radius, depth) {
+ccMesh.cone = function(radialSegments, depthSegments, startRadius, endRadius, depth) {
 	var geometry = new THREE.Geometry();
     
 	for(var i = 0.0; i <= depthSegments; i+=1.0)
     {
         var depthProgress = (i / depthSegments) * depth
-        var currentRadius = Math.lerp(radialSegments, 0.0, i / depthSegments)
-        
+        var currentRadius = Math.lerp(startRadius, endRadius, 1.0 - i / depthSegments)
+        console.log(currentRadius)
 		for(var segment = 0.0; segment <= radialSegments; segment++)
         {
             var angle = (segment / radialSegments) * Math.PI * 2.0
@@ -76,37 +76,6 @@ ccMesh.cone = function(radialSegments, depthSegments, radius, depth) {
 		}		
 	}
     
-	/*for(var i = 0.0; i <= depthSegments; i+=1.0)
-    {
-		for(var segment = 0.0; segment < radialSegments; segment++)
-        {
-			var f1 = i*radialSegments + segment
-            var f2 = (i+1)*radialSegments + segment
-            
-            geometry.faces.push(
-				new THREE.Face3(f1, f2, f2 + 1),
-				new THREE.Face3(f1, f2 + 1, f1 + 1)
-			);
-            
-            var uvxp1 = (segment / radialSegments)
-            var uvyp1 = i/depthSegments
-            var uvxp2 = ((segment+1) / radialSegments)
-            var uvyp2 = (i+1)/depthSegments
-            
-			geometry.faceVertexUvs[0].push([
-				new THREE.Vector2( uvxp1, uvyp1 ),
-				new THREE.Vector2( uvxp1, uvyp2 ),
-				new THREE.Vector2( uvxp2, uvyp2 )
-			  ],
-										  [
-				
-            new THREE.Vector2( uvxp1, uvyp1),
-            new THREE.Vector2( uvxp2, uvyp2 ),
-            new THREE.Vector2( uvxp2, uvyp1 )
-          ]);
-    }		
-	}*/
-	
 	geometry.uvsNeedUpdate = true;
 	geometry.center();
 	return geometry;
--- fitc/tunnelController.js
+++ fitc/tunnelController.js
@@ -2,26 +2,30 @@ function tunnelController(base){
 	this.base = base;
 }
 
+var uniforms;
+var material;
+
 tunnelController.prototype.init = function(){
-	var geometry = ccMesh.cone(10.0,10.0,10.0,10.);
+	var geometry = ccMesh.cone(20.0,50.0,10.0,.2,50.);
 	
-    var uniforms = {
-        time:       { value: 1.0 },
+    uniforms = {
+        time:       { type: "f", value: 1.0 }
     };
     
-	var material = new THREE.ShaderMaterial( {
+	material = new THREE.ShaderMaterial( {
         uniforms: uniforms,
 		vertexShader: document.getElementById( 'vertexShader' ).textContent,
         fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
-        side:THREE.DoubleSide
-	} );
+        wireframe:true
+    } );
     
 	var mesh = new THREE.Mesh(geometry, material);
 	base.scene.add(mesh);
-	mesh.position.z = -20;
+	mesh.position.z = -36;
 
 	base.addUpdateCallback(()=>{
-		mesh.rotation.x += .01;
-		mesh.rotation.y += .02;
+        uniforms.time.value += 0.1;
+		//mesh.rotation.x += .01;
+		//mesh.rotation.y += .02;
 	});
 }
