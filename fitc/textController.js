8b8881cffb384d57b901fbc181db04e0998edef7
connorbell <connor.n.bell@gmail.com>
2017-08-02 18:25:11 -0300

    Fixed a little text shader thing

--- fitc/textController.js
+++ fitc/textController.js
@@ -2,9 +2,22 @@ var font;
 
 function textController(base){
 	this.base = base;
+    var uniforms = {
+					time:       { value: 1.0 },
+					resolution: { value: new THREE.Vector2() }
+				};
+    console.log(document.getElementById( 'fontFragmentShader' ).textContent)
     this.material = new THREE.MultiMaterial( [
-        new THREE.MeshBasicMaterial( { color: 0xff0000, shading: THREE.FlatShading } ), // front
-        new THREE.MeshBasicMaterial( { color: 0xff00ff, shading: THREE.SmoothShading } ) // side
+        new THREE.ShaderMaterial( {          
+        
+        uniforms: {
+            time: {type:"f", value: 1.0 }
+    	},
+        
+        vertexShader: document.getElementById( 'fontVertexShader' ).textContent,
+        fragmentShader: document.getElementById( 'fontFragmentShader' ).textContent,
+                                     } ), // front
+        new THREE.MeshBasicMaterial( { color: 0x999998, shading: THREE.SmoothShading } ) // side
     ] );
     this.textMesh = new THREE.Mesh();
 }
@@ -13,17 +26,22 @@ textController.prototype.loadFont = function(){
     var loader = new THREE.FontLoader(base.loadingManager);
     
     loader.load( "fonts/helvetiker_regular.typeface.js", function ( response ) {
-        console.log("loaded font");
         font = response;
     } , function ( ok ) {
-        
-        console.log("progress " + ok.loaded)
+
     } , function(error){
         console.log(error)
+    });
+
+    this.animation = new ccAnimate(base, this.textMesh, new THREE.Vector3( 0, 0, 1 ))
+        base.addUpdateCallback(()=>{
+        this.textMesh.rotateOnAxis(new THREE.Vector3(0,0,1), Math.sin(base.time.time)*0.0025 );
     });
 }
 
 textController.prototype.setText = function(text) {
+    this.animation.end();
+    
     var textGeo = new THREE.TextGeometry( text, {
         font: font,
 
@@ -32,7 +50,7 @@ textController.prototype.setText = function(text) {
         curveSegments: 4,
 
         bevelThickness: 1.5,
-        bevelSize: 2.0,
+        bevelSize: 1.0,
         bevelEnabled: true,
 
         material: 0,
@@ -47,4 +65,5 @@ textController.prototype.setText = function(text) {
     this.textMesh.position.z = -200;
 
     base.scene.add( this.textMesh );
+    this.animation.start();
 }
