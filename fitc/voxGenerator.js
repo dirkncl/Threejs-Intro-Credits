13db48200f54347f44d3e2fa18e3d6bd9b994152
cale <cale.bradbury@gmail.com>
2017-08-02 19:12:54 -0300

    just adding saving shit

--- fitc/voxGenerator.js
+++ fitc/voxGenerator.js
@@ -21,13 +21,28 @@ voxGenerator = function(base, sounds){
 	function loadNextSound(){
 		base.loadingManager.itemStart("vox "+sounds[index]);
 		var j = Math.floor(Math.random()*v.length);
-		self.sound[sounds[index]] = (meSpeak.speak(sounds[index], {'rawdata':'array', speed:Math.random()*33+100, variant:v[j]}));
+		self.sound[sounds[index]] = (meSpeak.speak(sounds[index], {'rawdata':'lo', speed:Math.random()*33+100, variant:v[j]}));
+		//saveByteArray([self.sound[sounds[index]] ], sounds[index]+".wav");
 		base.loadingManager.itemEnd("vox "+sounds[index]);
 		index++;
 		if(index<sounds.length){
 			base.scheduler.callNextFrame(loadNextSound);
 		}
 	}	
+	
+	var saveByteArray = (function () {
+		var a = document.createElement("a");
+		document.body.appendChild(a);
+		a.style = "display: none";
+		return function (data, name) {
+			var blob = new Blob(data, {type: "octet/stream"}),
+				url = window.URL.createObjectURL(blob);
+			a.href = url;
+			a.download = name;
+			a.click();
+			window.URL.revokeObjectURL(url);
+		};
+	}());
 	
 	checkLoaded();
 }
